7f3b52db9725d86a33751b27e900273f1e682bad
potatomaster <56097975+potatomasqer@users.noreply.github.com>
2020-03-02 11:14:00 -0600

    added lower card amount support

--- Poker/Poker/AI.swift
+++ Poker/Poker/AI.swift
@@ -15,7 +15,7 @@ class AI {
         
         
         func chanceOfBetterCard(cardToCheck: Int, card2: Int, card3: Int, card4: Int, card5: Int)-> Int{
-            
+            if card5 != nil{
             if cardToCheck == card2 && cardToCheck == card3 && cardToCheck == card4{
                 //quad
                 return 6
@@ -34,7 +34,7 @@ class AI {
             }else if (cardToCheck - 1) == card2 && (card2 - 1) == card3 && (card3 - 1) == card4 && (card4 - 1) == card5{
                 //straight
                 return 3
-            }else if cardToCheck%4 == card2%4 && cardToCheck%4 == card3%4 && cardToCheck%4 == card5%4{
+            }else if cardToCheck%4 == card2%4 && cardToCheck%4 == card3%4 && cardToCheck%4 == card4%4 && cardToCheck%4 == card5{
                 //flush
                 return 4
             }else if cardToCheck == card2  && cardToCheck == card3{
@@ -68,10 +68,65 @@ class AI {
                 //pare
                 return 1
             }else{
-            
-            return 0
+                return 0
+                }
+
+            }else if card4 != nil{
+                if ((cardToCheck%13 - 1) == card2%13 && (card2%13 - 1) == card3%13 && (card3%13 - 1) == card4%13) && (cardToCheck%4 == card2%4 && cardToCheck%4 == card3%4 && cardToCheck%4 == card4%4){
+                    //straight flush
+                    return 5
+                }else if (cardToCheck - 1) == card2 && (card2 - 1) == card3 && (card3 - 1) == card4{
+                    //straight
+                    return 3
+                }else if cardToCheck%4 == card2%4 && cardToCheck%4 == card3%4 && cardToCheck%4 == card4%4{
+                    //flush
+                    return 4
+                }else if cardToCheck == card2  && cardToCheck == card3{
+                    //triplet
+                    return 2
+                }else if cardToCheck == card2  && cardToCheck == card4{
+                    //triplet
+                    return 2
+                }else  if cardToCheck == card3  && cardToCheck == card4{
+                    //triplet
+                    return 2
+                }else  if cardToCheck == card2{
+                    //pare
+                    return 1
+                }else if cardToCheck == card3{
+                    //pare
+                    return 1
+                }else if cardToCheck == card4{
+                    //pare
+                    return 1
+                }else {
+                    return 0
+                    }
+            }else if card3 != nil{
+            if ((cardToCheck%13 - 1) == card2%13 && (card2%13 - 1) == card3%13 ) && (cardToCheck%4 == card2%4 && cardToCheck%4 == card3%4){
+                //straight flush
+                return 5
+            }else if (cardToCheck - 1) == card2 && (card2 - 1) == card3 {
+                //straight
+                return 3
+            }else if cardToCheck%4 == card2%4 && cardToCheck%4 == card3%4 {
+                //flush
+                return 4
+            }else if cardToCheck == card2  && cardToCheck == card3{
+                //triplet
+                return 2
+            }else   if cardToCheck == card2{
+                //pare
+                return 1
+            }else if cardToCheck == card3{
+                //pare
+                return 1
+            }else  {
+                return 0
+                }
             }
-        }
+            return 0
+        } 
         func cardChecker(card1: Int, card2: Int, card3: Int, card4: Int, card5: Int)-> Int{
            //combos
             // proportion of card # and any card
