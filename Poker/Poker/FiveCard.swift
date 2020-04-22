c6ecf9582711a5cca3a7312e7a5caf5d9be63ff3
potatomasqer <56097975+potatomasqer@users.noreply.github.com>
2020-04-22 13:50:22 -0500

    Update FiveCard.swift

--- Poker/Poker/FiveCard.swift
+++ Poker/Poker/FiveCard.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class FiveCard: UIViewController {
+    var aiControler = AI()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +17,4 @@ class FiveCard: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
